b468ab6d7d2a6b52c0aa103e1b3012cbf67bf8da
Akshay <goyalakshay14.ag@gmail.com>
2019-05-12 17:40:04 -0400

    update msg

--- simple-java-maven-app/src/main/java/com/mycompany/app/App.java
+++ simple-java-maven-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World to Infoblox!";
+    private final String message = "Hello World to Infoblox";
     public static Integer add(Integer a, Integer b) {
         if (a == null) {
             return null;
