9e446ca511be66378253f49aaaf1b4b274627fe1
Akshay <goyalakshay14.ag@gmail.com>
2019-05-12 03:28:41 -0400

    message update\

--- simple-java-maven-app/src/test/java/com/mycompany/app/AppTest.java
+++ simple-java-maven-app/src/test/java/com/mycompany/app/AppTest.java
@@ -40,6 +40,19 @@ public class AppTest
         }
     }
 
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        Integer a = 15;
+        Integer b = 15;
+        Integer expResult = 30;
+        Integer result = Util.add(a, b);
+        assertEquals(expResult, result);
+        assertEquals(null, Util.add(15, null));
+        assertEquals(null, Util.add(null, 15));
+        assertEquals(null, Util.add(null, null));
+    }
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
