e21acce5a2513cd928925dbbdf7603ce4669db3f
Akshay <goyalakshay14.ag@gmail.com>
2019-05-12 03:12:09 -0400

    msg update

--- simple-java-maven-app/src/main/java/com/mycompany/app/App.java
+++ simple-java-maven-app/src/main/java/com/mycompany/app/App.java
@@ -7,15 +7,26 @@ public class App
 {
 
     private final String message = "Hello World to Infoblox!";
+   
+    private final Integer sum()
+    {
+         int a =10, b=20;
+         int c;
+         c = a+b;
+    }
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(new App().getSum());
     }
 
     private final String getMessage() {
         return message;
+    }
+    private final Integer getSum() {
+        return sum();
     }
 
 }
